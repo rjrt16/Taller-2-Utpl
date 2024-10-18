1360722d434086fa2a291603e0754fc8caaafa8d
rjr16 <rickyrjrt@gmail.com>
2024-10-17 19:51:56 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -1,7 +1,8 @@
 package proyecto01;
 public class Proyecto01 {
     public static void main(String[] args) {
-        System.out.println("Ejemplo basico de java versionado a traves dde git manejado desde github");
+        System.out.println("Ejemplo basico de java versionado a traves dde git manejado desde github creado por rjrt16");
+        
     }
     
 }
