8f01f6f0ed6574a3a5c347bcae3c2749aaa3c321
rjr16 <rickyrjrt@gmail.com>
2024-10-17 19:44:37 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,7 @@
+package proyecto01;
+public class Proyecto01 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo basico de java versionado a traves dde git manejado desde github");
+    }
+    
+}
