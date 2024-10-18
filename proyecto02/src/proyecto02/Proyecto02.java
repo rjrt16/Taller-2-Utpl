16b2f8bb4eca659f354ea44187219607a78403ac
rjr16 <rickyrjrt@gmail.com>
2024-10-17 20:00:09 -0500

    tercer commit, trabajando con git y githubclassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,8 @@
+package proyecto02;
+
+public class Proyecto02 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos basico de java versionado a traves de git manjado desde github ademas se usar githubclassroom");
+    }
+    
+}
